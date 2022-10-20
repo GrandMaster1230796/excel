09878ec2c4f843abd3431fceaa127cacb687fbd0
GrandMaster1230796 <hretshrets@gmail.com>
2022-10-20 02:25:41 +0200

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace myexcel
+{
+    partial class Excel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.add_row = new System.Windows.Forms.Button();
+            this.add_col = new System.Windows.Forms.Button();
+            this.remove_row = new System.Windows.Forms.Button();
+            this.remove_col = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Save file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // add_row
+            // 
+            this.add_row.Location = new System.Drawing.Point(12, 41);
+            this.add_row.Name = "add_row";
+            this.add_row.Size = new System.Drawing.Size(75, 23);
+            this.add_row.TabIndex = 1;
+            this.add_row.Text = "Add row";
+            this.add_row.UseVisualStyleBackColor = true;
+            this.add_row.Click += new System.EventHandler(this.add_row_Click);
+            // 
+            // add_col
+            // 
+            this.add_col.Location = new System.Drawing.Point(93, 41);
+            this.add_col.Name = "add_col";
+            this.add_col.Size = new System.Drawing.Size(97, 23);
+            this.add_col.TabIndex = 2;
+            this.add_col.Text = "Add column";
+            this.add_col.UseVisualStyleBackColor = true;
+            this.add_col.Click += new System.EventHandler(this.add_col_Click);
+            // 
+            // remove_row
+            // 
+            this.remove_row.Location = new System.Drawing.Point(196, 41);
+            this.remove_row.Name = "remove_row";
+            this.remove_row.Size = new System.Drawing.Size(100, 23);
+            this.remove_row.TabIndex = 3;
+            this.remove_row.Text = "Remove row";
+            this.remove_row.UseVisualStyleBackColor = true;
+            this.remove_row.Click += new System.EventHandler(this.remove_row_Click);
+            // 
+            // remove_col
+            // 
+            this.remove_col.Location = new System.Drawing.Point(302, 41);
+            this.remove_col.Name = "remove_col";
+            this.remove_col.Size = new System.Drawing.Size(123, 23);
+            this.remove_col.TabIndex = 4;
+            this.remove_col.Text = "Remove column";
+            this.remove_col.UseVisualStyleBackColor = true;
+            this.remove_col.Click += new System.EventHandler(this.remove_col_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 98);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(907, 415);
+            this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
+            this.dataGridView1.RowPostPaint += new System.Windows.Forms.DataGridViewRowPostPaintEventHandler(this.dataGridView1_RowPostPaint);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(93, 70);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(413, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.textBox2.Location = new System.Drawing.Point(12, 70);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(75, 22);
+            this.textBox2.TabIndex = 7;
+            // 
+            // Excel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(973, 525);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.remove_col);
+            this.Controls.Add(this.remove_row);
+            this.Controls.Add(this.add_col);
+            this.Controls.Add(this.add_row);
+            this.Controls.Add(this.button1);
+            this.Name = "Excel";
+            this.ShowIcon = false;
+            this.Text = "Excel";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button add_row;
+        private System.Windows.Forms.Button add_col;
+        private System.Windows.Forms.Button remove_row;
+        private System.Windows.Forms.Button remove_col;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClosedXML.Excel;
+using Newtonsoft.Json;
+using org.mariuszgromada.math.mxparser;
+
+namespace myexcel
+{
+    public partial class Excel : Form
+    {
+        public Excel()
+        {
+            InitializeComponent();
+            GridSetup();
+        }
+        DataTable table = new DataTable();
+        string[,,] id_values =  new string[100,100,2];
+        int row_num = 0;
+        int col_num = 0;
+
+        private void GridSetup()
+        {
+            if (col_num == 0)
+            {
+                while (row_num < 4)
+                {
+                    table.Columns.Add();
+                    col_num += 1;
+                    table.Rows.Add();
+                    row_num += 1;
+                }
+                for (int x = 0; x < id_values.GetLength(0); x++)
+                {
+                    for (int y = 0; y < id_values.GetLength(1); y++)
+                    {   
+                        for(int z = 0; z < id_values.GetLength(2); z++)
+                        id_values[x, y, z] = "";
+                    }
+                }
+            }
+            dataGridView1.DataSource = table;
+        }
+        private void add_row_Click(object sender, EventArgs e)
+        {
+            table.Rows.Add();
+            row_num += 1;
+        }
+
+        private void add_col_Click(object sender, EventArgs e)
+        {
+            table.Columns.Add();
+            col_num += 1;
+        }
+        private void dataGridView1_RowPostPaint(object sender, DataGridViewRowPostPaintEventArgs e)
+        {
+            var grid = sender as DataGridView;
+            var rowIdx = (e.RowIndex + 1).ToString();
+
+            var centerformat = new StringFormat()
+            {
+                Alignment = StringAlignment.Center,
+                LineAlignment = StringAlignment.Center,
+            };
+            var headerBounds = new Rectangle(e.RowBounds.Left, e.RowBounds.Top, grid.RowHeadersWidth, e.RowBounds.Height);
+            e.Graphics.DrawString(rowIdx, this.Font, SystemBrushes.ControlText, headerBounds, centerformat);
+        }
+
+        private void remove_row_Click(object sender, EventArgs e)
+        {
+            if (row_num > 1)
+            {
+                int curr_row = dataGridView1.CurrentCell.RowIndex;
+                int x = curr_row;
+                if (x == -1) x = 0;
+                for (; x < id_values.GetLength(0) - 1; x++)
+                {
+                    for (int y = 0; y < id_values.GetLength(1); y++)
+                    {
+                        for (int z = 0; z < 2; z++)
+                            id_values[x, y, z] = id_values[x+1, y ,z];
+                    }
+                }
+                table.Rows.RemoveAt(curr_row);
+                row_num -= 1;
+            }
+        }
+        private void remove_col_Click(object sender, EventArgs e)
+        {
+            if (col_num > 1)
+            {
+                int curr_col = dataGridView1.Columns.Count-1;
+                for (int x = 0; x < id_values.GetLength(0); x++)
+                {
+                    for (int z = 0; z < id_values.GetLength(2); z++)
+                        id_values[x, curr_col, z] = "";
+                }
+                table.Columns.RemoveAt(curr_col);
+                col_num -= 1;
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.ColumnIndex >= 0)
+            {
+                textBox1.Text = id_values[e.RowIndex, e.ColumnIndex, 0] + "";
+                textBox2.Text = "r" + (e.RowIndex+1).ToString() + "c" + (e.ColumnIndex+1).ToString();
+            }
+        }
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView1.CurrentCell.Value.ToString() != "")
+            {
+                id_values[e.RowIndex, e.ColumnIndex, 0] = dataGridView1.CurrentCell.Value.ToString();
+                string base_str = dataGridView1.CurrentCell.Value.ToString();
+                if (base_str.IndexOf("r") != -1)
+                {
+                    string final_str = base_str;
+                    while (final_str.IndexOf("r") != -1)
+                    {
+                        string temp1_str = final_str.Substring(0, final_str.IndexOf("r"));
+                        string temp2_str = final_str.Substring(final_str.IndexOf("r") + 1, final_str.IndexOf("c") - final_str.IndexOf("r") - 1);
+                        string temp3_str = final_str.Substring(final_str.IndexOf("c") + 1, final_str.Length - final_str.IndexOf("c") - 1);
+                        string temp4_str;
+                        if (temp3_str.IndexOf(" ") == -1)
+                        {
+                            temp4_str = temp3_str.Substring(0, temp3_str.Length);
+                        }
+                        else
+                        {
+                            temp4_str = temp3_str.Substring(0, temp3_str.IndexOf(" "));
+                        }
+                        string t2_trimmed = String.Concat(temp2_str.Where(c => !Char.IsWhiteSpace(c)));
+                        string t4_trimmed = String.Concat(temp4_str.Where(c => !Char.IsWhiteSpace(c)));
+                        //string temp5_str = string.Concat(temp1_str, id_values[Int32.Parse(t2_trimmed) - 1, Int32.Parse(t4_trimmed) - 1, 1], temp3_str.Substring(temp3_str.IndexOf(" "), temp3_str.Length - temp3_str.IndexOf(" ")));
+                        final_str = string.Concat(temp1_str, id_values[Int32.Parse(t2_trimmed) - 1, Int32.Parse(t4_trimmed) - 1, 1], temp3_str.Substring(temp3_str.IndexOf(" "), temp3_str.Length - temp3_str.IndexOf(" ")));
+                        //final_str = String.Concat(temp5_str.Where(c => !Char.IsWhiteSpace(c)));
+                    }
+                   // dataGridView1.CurrentCell.Value = final_str;
+                    Expression exp = new Expression(final_str);
+                    dataGridView1.CurrentCell.Value = exp.calculate().ToString();
+                    id_values[e.RowIndex, e.ColumnIndex, 1] = dataGridView1.CurrentCell.Value.ToString();
+                    textBox1.Text = exp.getExpressionString()+"";
+                    id_values[e.RowIndex, e.ColumnIndex, 0] = exp.getExpressionString();
+                }
+                else
+                {
+                    Expression exp = new Expression(dataGridView1.CurrentCell.Value.ToString());
+                    dataGridView1.CurrentCell.Value = exp.calculate().ToString();
+                    id_values[e.RowIndex, e.ColumnIndex, 1] = dataGridView1.CurrentCell.Value.ToString();
+                    textBox1.Text = exp.getExpressionString()+"";
+                    id_values[e.RowIndex, e.ColumnIndex, 0] = exp.getExpressionString();
+                }
+            }
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText("C:\\Users\\gorle\\Downloads\\excel.txt", JsonConvert.SerializeObject(id_values));
+        }
+    }
+}
